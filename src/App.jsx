cbcf10119ba1d913dec874f9915ef23da12a71c9
ayayam <aya.s.yamamoto@gmail.com>
2023-08-22 21:01:42 -0400

    Props, had to delete any instance of eslint because props validation using PropTypes library does not work

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import * as React from 'react';
-import './App.css'
-
+import './App.css';
 
 // const welcome = {
 //   title: 'React',
@@ -14,26 +13,27 @@ import './App.css'
 //   return title;
 // }
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
 
-const App = () => (
+  return (
     <div>
       <h1>My Hacker Stories</h1>
 
@@ -41,36 +41,49 @@ const App = () => (
 
       <hr />
 
-      <List />
-
-      <List />
+      <List list={stories} />
 
     </div>
-  );
+  );  
+}
 
-const Search = () => (
+
+const Search = () => {
+  const handleChange = (event) => {
+    // synthetic event
+    console.log(event);
+    // value of target (here: input HTML element)
+    console.log(event.target.value);
+  };
+
+  return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text"></input>
+      <input id="search" type="text" onChange={handleChange}></input>
     </div>
   );
+}
 
-const List = () => (
-    <ul>
-      {list.map((item) => (
-        <li key={item.objectID}>
-          <span>
-            <a href={item.url}><i>{item.title}</i></a>
-          </span>
-          <ul>
-            <li><span> by {item.author}</span></li>
-            <li><span> Comments: {item.num_comments}</span></li>
-            <li><span> Points: {item.points}</span></li>
-          </ul>
-          
-        </li>
-      ))}
+const List = (props) => (
+  <ul>
+    {props.list.map((item) => (
+      <Item key={item.objectID} item={item} />
+    ))}
   </ul>
-  );
+);
+
+const Item = (props) => (
+  <li>
+    <span>
+      <a href={props.item.url}><i>{props.item.title}</i></a>
+    </span>
+  <ul>
+    <li><span> by {props.item.author}</span></li>
+    <li><span> Comments: {props.item.num_comments}</span></li>
+    <li><span> Points: {props.item.points}</span></li>
+  </ul>
+  
+</li>
+);
 
 export default App
